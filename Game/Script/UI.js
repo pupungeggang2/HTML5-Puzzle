d6f7ef8b4a2e4a7fa799e9e82692a5e6bcc1ed8b
pupungeggang2 <kyongkyong2892@gmail.com>
2024-11-18 00:41:08 +0900

    Start

--- Game/Script/UI.js
+++ Game/Script/UI.js
@@ -5,12 +5,13 @@ const UI = {
 
     titleMenu: {
         titleText: [24, 24],
-        buttonStart: [],
+        buttonStart: [160, 160, 960, 80],
+        textStart: [164, 184],
         
     },
 
     game: {
+        buttonMenu: [1220, 20, 40, 40],
         titleText: [24, 24],
-          
     },
 }
